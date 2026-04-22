08390cfe16f00e6aa52d416de6d7bcb590c302f2
stefan-cde <stefan.iacob@productsup.com>
2026-04-22 09:21:29 +0300

    Use connector:run:import command name

--- src/DataSource/Infrastructure/Cli/RunImportCommand.php
+++ src/DataSource/Infrastructure/Cli/RunImportCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'container:run:import')]
+#[AsCommand(name: 'connector:run:import')]
 class RunImportCommand extends Command
 {
     public function __construct(
