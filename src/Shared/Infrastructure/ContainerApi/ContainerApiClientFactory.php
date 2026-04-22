190ed248588c64b32c1fb21875b7260a7a47a9cc
stefan-cde <stefan.iacob@productsup.com>
2026-04-22 09:18:32 +0300

    Add container.api service definition and factories

---
+++ src/Shared/Infrastructure/ContainerApi/ContainerApiClientFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\ContainerApi;
+
+use GuzzleHttp\Client as HttpClient;
+use Productsup\CDE\ContainerApi\BaseClient\Client;
+
+class ContainerApiClientFactory
+{
+    private const int REQUEST_TIMEOUT = 3600;
+    private const int CONNECT_TIMEOUT = 30;
+
+    public function __invoke(): Client
+    {
+        $httpClient = new HttpClient([
+            'base_uri'        => 'http://cde-container-api',
+            'timeout'         => self::REQUEST_TIMEOUT,
+            'connect_timeout' => self::CONNECT_TIMEOUT,
+        ]);
+
+        return Client::create($httpClient);
+    }
+}
---
+++ src/Shared/Infrastructure/ContainerApi/ContainerApiFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\ContainerApi;
+
+use Productsup\CDE\ContainerApi\BaseClient\Client;
+use Productsup\CDE\ContainerApi\ContainerApi;
+use Productsup\CDE\ContainerApi\ContainerApiInterface;
+
+readonly class ContainerApiFactory
+{
+    public function __invoke(Client $containerApiClient): ContainerApiInterface
+    {
+        return new ContainerApi($containerApiClient);
+    }
+}
